8fb90e096d0219ea701b4faa2056978fdefcdb0d
jiwonidaax-x <jgb07012@gmail.com>
2022-10-26 03:54:15 -0400

    second

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,14 +1,14 @@
 import { Status } from "./Status";
 export const Card=(props)=>{
     return (
-        <article className="w-96 h-fit rounded-lg shadow-xl my-8 mx-auto">
+        <article className="align-text-top w-96 h-fit rounded-lg shadow-xl my-10 mx-10 inline-block ">
             <img 
             className="rounded-t-lg"
               alt="cardImg"
               src={props.Img}
             />
           <div className="p-8"> 
-           <Status status={props.tag}/>
+          <Status status={props.tag}/>
             <h3 className="text-3xl mt-2 font-extrabold">{props.title}</h3>
             <div>
               <p className="mt-3 text-gray-400 font-semibold">
