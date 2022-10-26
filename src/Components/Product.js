a52e5868542573580433b79d6452dc50ede1484f
jiwonidaax-x <jgb07012@gmail.com>
2022-10-26 15:57:06 -0400

    card

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,7 +1,5 @@
 export const Product = (props) => {
-  //상품 데이터 패치
   if (props.product === true) {
-    //list template
     if (props.type === "list") {
       return (
         <ul className="p-8">
@@ -12,12 +10,11 @@ export const Product = (props) => {
               src="https://t1.daumcdn.net/friends/prod/product/20201020152815054_8809721505908_AW_00.jpg?type=thumb&opt=R103x103@2xa"
             />
             <div className=" float-right py-2 mr-7">
-              <p className="text-gray-500 font-semibold">
-                구름폭신필로우_라이언
-              </p>
+              <p className="text-gray-500 font-500">구름폭신필로우_라이언</p>
               <p className="text-left font-bold text-lg">39,000원</p>
               <button>
                 <img
+                  alt="btn"
                   src="https://t1.kakaocdn.net/friends/new_store/2.0/common/basket-pink-3.png"
                   width="37px"
                 />
@@ -28,17 +25,27 @@ export const Product = (props) => {
       );
     } else if (props.type === "card") {
       return (
-        <ul>
-          <li className="item card">
+        <ul className="p-8">
+          <li className="w-32 inline-block ml-7">
             <img
+              width="140px"
               alt="item"
               src="https://t1.daumcdn.net/friends/prod/product/20201020152815054_8809721505908_AW_00.jpg?type=thumb&opt=R103x103@2xa"
             />
             <div className="content">
-              <div>
-                <p>구름폭신필로우_라이언</p>
-                <p className="price">39,000원</p>
-              </div>
+              <p className="mt-3 text-gray-500 font-500 inline-block">
+                구름폭신필로우_라이언
+              </p>
+              <p className="text-left font-bold text-lg inline mr-3">
+                39,000원
+              </p>
+              <button>
+                <img
+                  alt="btn"
+                  src="https://t1.kakaocdn.net/friends/new_store/2.0/common/basket-pink-3.png"
+                  width="37px"
+                />
+              </button>
             </div>
           </li>
         </ul>
